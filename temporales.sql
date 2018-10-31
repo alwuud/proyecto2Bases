01416822b87af05677f7d013636b354941a5c39f
alwuud <dahh.drag@gmail.com>
2018-10-31 10:54:16 -0600

    Se crearon las tablas temporales para los archivos .csv

---
+++ temporales.sql
@@ -0,0 +1,35 @@
+CREATE TABLE TempJugadores (
+Equipo VARCHAR(128),
+Camisola INT,
+Posicion VARCHAR(10),
+Nombre VARCHAR(128),
+Fehca_nacimiento DATE,
+Altura INT,
+Peso INT ,
+Goles INT
+);
+
+
+CREATE TABLE TempEquipo
+(
+Equipo VARCHAR(128)	,
+Confederacion VARCHAR(128),
+Grupo VARCHAR(5)
+
+);
+
+
+
+CREATE TABLE TempCatJuegos
+(
+Fecha VARCHAR(20),
+Hora VARCHAR(20),
+Sede	VARCHAR(100),
+Arbitro_central VARCHAR(100),
+Asistente1 VARCHAR(100),
+Asistente2 VARCHAR(100),
+Equipo1 VARCHAR(100),
+Equipo2 VARCHAR(100)
+);
+
+
